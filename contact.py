0be2265162a06d53f2f8b689727307b554bde156
DariaMagarshak <55354330+DariaMagarshak@users.noreply.github.com>
2019-09-18 23:19:38 +0300

    Создали отдельный класс

---
+++ contact.py
@@ -0,0 +1,28 @@
+class Contact:
+    def __init__(self, wd, firstname, middlename, lastname, nickname, company, company_address, home_number,
+                       mobile_number, work_number, fax_number, email, email2, email3, homepage, bday, bmonth, byear,
+                       aday, amonth, ayear, address2, homephone, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.company_address = company_address
+        self.home_number = home_number
+        self.mobile_number = mobile_number
+        self.work_number = work_number
+        self.fax_number = fax_number
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.homephone = homephone
+        self.notes = notes
+
