628d89a79da9dca2278747fd60d5eabf78032deb
DariaMagarshak <55354330+DariaMagarshak@users.noreply.github.com>
2019-10-01 23:05:16 +0300

    оптимизация ввода данных в формы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,7 +1,19 @@
 from model.group import Group
 
 
-def test_modify_group(app):
+#def test_modify_group(app):
+   # app.session.login(username="admin", password="secret")
+   # app.group.modify(Group(name="w11", header="2222", footer="3333"))
+   # app.session.logout()
+
+
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(name="w11", header="2222", footer="3333"))
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
