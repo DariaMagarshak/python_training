e03a5cfc3b699d04c683703d860f606fb0cf6f63
DariaMagarshak <55354330+DariaMagarshak@users.noreply.github.com>
2019-10-01 23:36:32 +0300

    валидация фикстуры перед каждым тестом

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,9 +2,7 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify(Contact(firstname="1", middlename="2", lastname="3", nickname="4", company="4",
                                company_address="5", home_number="6", mobile_number="7", work_number="8", fax_number="9", email="1",
                                email2="2", email3="3", homepage="4", bday="5", bmonth="April", byear="1920", aday="5",
                                amonth="April", ayear="1950", address2="1", homephone="1", notes="1"))
-    app.session.logout()
