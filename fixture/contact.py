97be69c620cb83d3e400350735fea4b669703549
DariaMagarshak <55354330+DariaMagarshak@users.noreply.github.com>
2019-10-20 17:40:34 +0300

    Задание 10-2

--- fixture/contact.py
+++ fixture/contact.py
@@ -101,7 +101,9 @@ class ContactHelper:
     def return_to_home_page(self):
          # self.app.open_home_page()
          wd = self.app.wd
-         wd.find_element_by_link_text("home").click()
+         if not (wd.current_url.endswith("/addressbook/") and len(
+                 wd.find_elements_by_xpath("(//img[@alt='Edit'])")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
