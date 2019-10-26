18ee0e34d80c98ba00f9d58f091b2f84241f70e9
DariaMagarshak <55354330+DariaMagarshak@users.noreply.github.com>
2019-10-27 01:05:37 +0300

    задание 17

--- conftest.py
+++ conftest.py
@@ -1,25 +1,31 @@
 # задание 13
 import pytest
 from fixture.application import Application
-
+import json
+import os.path
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     # получаем доступ к сохраненному параметру через объект request
-    browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    username = request.config.getoption("--username")
-    password = request.config.getoption("--password")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                   request.config.getoption("--target"))
+        browser = request.config.getoption("--browser")
+        __file__
+        with open(config_file) as f:
+           target = json.load(f)
+    #base_url = request.config.getoption("--baseUrl")
+    #username = request.config.getoption("--username")
+    #password = request.config.getoption("--password")
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
     #fixture.session.ensure_login(username="admin", password="secret")
-    fixture.session.ensure_login(username=username, password=password)
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -34,6 +40,7 @@ def stop(request):
 def pytest_addoption(parser):
 #параметр browser, действие "сохранить параметр", дефолтное значение
     parser.addoption("--browser", action="store", default = "firefox")
-    parser.addoption("--baseUrl", action="store", default = "http://localhost/addressbook/")
-    parser.addoption("--username", action="store", default = "admin")
-    parser.addoption("--password", action="store", default = "secret")
+    #parser.addoption("--baseUrl", action="store", default = "http://localhost/addressbook/")
+    #parser.addoption("--username", action="store", default = "admin")
+    #parser.addoption("--password", action="store", default = "secret")
+    parser.addoption("--target", action="store", default = "target.json")
