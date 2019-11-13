7bfafddc28b55ba657ca68430cc4af1f6315dec2
DariaMagarshak <55354330+DariaMagarshak@users.noreply.github.com>
2019-11-13 23:44:54 +0300

    добавление контакта в группу

--- test/add_contact_to_group.py
+++ test/add_contact_to_group.py
@@ -14,51 +14,40 @@ def test_add_contact_to_group(app, orm):
         app.group.create(Group(name="test"))
 
 
-
     for gr_indx in range(len(app.group.get_group_list())):
-
+        # получаем группу по текущему индексу
         current_group = app.group.get_group_list()[gr_indx]
+        # получаем id текущей группы
         gr_id = str(current_group.id)
+        # проверяем, список контактов, которые не привязаны к текущей группе
         contact_not_in_group = list(orm.get_contacts_not_in_group(Group(id="%s" % gr_id)))
+        # получаем начальный список контактов, привязанных к текущей группе
+        old_info = orm.get_contacts_in_group(Group(id="%s" % gr_id))
+        # если есть контакты, которые еще не привязаны к группе (список не пустой)
         if contact_not_in_group !=[]:
+            # получаем идентификатор текущей группы
             c_id = contact_not_in_group[0].id
-            old_info = orm.get_contact_info(Group(id="%s" % c_id))
+            # добавляем контакт с идентификатором c_id в текущую группу
             app.contact.add_contact_to_group(c_id, current_group)
-            new_info = orm.get_contact_info(Group(id="%s" % c_id))
-            group_from_db = orm.get_group_list()[gr_indx]
-            old_info.append(group_from_db)
-        else:
-            new_contact = app.contact.create(Contact(firstname="New contact2"))
-            c_id = app.contact.get_contact_list()[len(app.group.get_group_list())+1].id
-            old_info = orm.get_contact_info(Group(id="%s" % str(c_id)))
-            app.contact.add_contact_to_group(c_id, current_group)
-            new_info = orm.get_contact_info(Group(id="%s" % str(c_id)))
-            group_from_db = orm.get_group_list()[gr_indx]
-            old_info.append(group_from_db)
+            # получаем новый список контактов, привязанных к текущей группе
+            new_info = orm.get_contacts_in_group(Group(id="%s" % gr_id))
+            # в начальный список контактов, привязанных к текущей группе, добавляем контакт с идентификатором c_id
+            old_info.append(contact_not_in_group[0])
 
+        # если все имеющиеся контакты уже привязаны к группе (список пустой)
+        elif gr_indx==-1:
+                # создаем новый контакт
+                app.contact.create(Contact(firstname="New contact3"))
+                # получаем его идентификатор - "-1", т.к. контакт должен появиться в конце списка
+                c_id = app.contact.get_contact_list()[-1].id
+                # добавляем контакт с идентификатором c_id в текущую группу
+                app.contact.add_contact_to_group(c_id, current_group)
+                # получаем новый список контактов, привязанных к текущей группе
+                new_info = orm.get_contacts_in_group(Group(id="%s" % gr_id))
+                new_contact_from_db = orm.get_contact_list()[-1]
+                # в начальный список контактов, привязанных к текущей группе, добавляем new_contact_from_db
+                old_info.append(new_contact_from_db)
+        # сравниваем отсортированные по идентификатору измененный начальный и новый списки контактов, привязанных к текущей группе
         assert sorted(old_info, key=Contact.id_or_max) == sorted(new_info, key=Contact.id_or_max)
 
-
-
-
-
-
-
-
-   # index_c = randrange(len(app.contact.get_contact_list()))
-    #index_g = randrange(len(app.group.get_group_list()))
-
-    #random_group = app.group.get_group_list()[index_g]
-    #random_contact = app.contact.get_contact_list()[index_c]
-    #id_group = random_group.id
-    #id_contact = random_contact.id
-
-    #old_info = orm.get_contact_info(Group(id="%s" % c_id))
-    # добавляем случайный контакт в случайную группу
-    #app.contact.add_contact_to_group(index_c, random_group)
-    #new_info = orm.get_contact_info(Group(id="%s" % c_id))
-    #old_info.append(random_group)
-
-    # сравниваем старый и новый списки
-   # assert sorted(old_info, key=Contact.id_or_max) == sorted(new_info, key=Contact.id_or_max)
 
