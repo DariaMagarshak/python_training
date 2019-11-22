f20664d5e3aad6141bb573ee631b55cd20077720
DariaMagarshak <55354330+DariaMagarshak@users.noreply.github.com>
2019-11-23 01:47:30 +0300

    задание 29

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,8 +1,9 @@
 # задание 13
 from model.group import Group
-#import pytest
-#from data.groups import testdata as testdata
+import pytest
 
+
+#from data.groups import testdata as testdata
 
 
 #testdata = [
@@ -16,13 +17,16 @@ from model.group import Group
 def test_add_group(app, db, json_groups):
     #для того, чтобы можно было продолжать работать с переменной group
     group = json_groups
-    old_groups = db.get_group_list()
+    with pytest.allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step('When I add a group %s to the list'%group):
     #group = Group(name="sss", header="ssss", footer="sssddd")
-    app.group.create(group)
+        app.group.create(group)
     #assert len(old_groups) + 1 == app.group.count()
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with pytest.allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 #def test_add_empty_group(app):
